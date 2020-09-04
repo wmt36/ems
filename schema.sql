d9537ec966ef112bcf96471920cc85ff11b9e12c
wmt36 <wmt36@Williams-iMac.local>
2020-09-04 12:28:24 -0600

    updated version of code for more help with mysql

--- schema.sql
+++ schema.sql
@@ -1,34 +1,41 @@
 DROP DATABASE  IF EXISTS    employee_listDB;
-CREATE database employee_listDB;
+
+
+CREATE DATABASE employee_listDB;
 
 
 USE employee_listDB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NULL, 
-    PRIVATE KEY (id)   
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    depatment_name VARCHAR(30) NOT NULL 
 
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NULL,
-    salary DECIMAL(10,4) NULL,
-    department_id INT NULL,
-    PRIVATE KEY (id)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,4) NOT NULL,
+    department_id INT NOT NULL,
+    CONSTRAINT FK_department FOREIGN KEY (department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     frst_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
-    PRIVATE KEY (id)
+    CONSTRAINT FK_role FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    CONSTRAINT FK_employee FOREIGN KEY (manager_id)
+    REFERENCES role(id)
 
 );
 
 SELECT * FROM department;
 SELECT * FROM role;
-SELECT * FROM employee;
+SELECT * FROM employee; 
+
+
