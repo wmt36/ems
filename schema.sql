41ef8c7432a6dd817084ef7ef9066edc7fef384c
wmt36 <wmt36@Williams-iMac.local>
2020-09-04 22:06:17 -0600

    changes made for help

--- schema.sql
+++ schema.sql
@@ -16,7 +16,8 @@ CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    INDEX dep_ind (department_id),
     CONSTRAINT FK_department FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
@@ -27,8 +28,10 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
+    INDEX role_ind (role_id),
     CONSTRAINT FK_role FOREIGN KEY (role_id)
     REFERENCES role(id),
+    INDEX man_ind (manager_id),
     CONSTRAINT FK_employee FOREIGN KEY (manager_id)
     REFERENCES role(id)
 
@@ -39,3 +42,4 @@ SELECT * FROM role;
 SELECT * FROM employee; 
 
 
+Error Code: 1075. Incorrect table definition; there can be only one auto column and it must be defined as a key
