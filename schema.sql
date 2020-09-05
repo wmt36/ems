a8c61b5610768deea27e03a564557d93890292d3
wmt36 <wmt36@Williams-iMac.local>
2020-09-05 09:33:40 -0600

    new pushes for help from bcs

--- schema.sql
+++ schema.sql
@@ -16,24 +16,23 @@ CREATE TABLE role (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
-    department_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    department_id INT UNSIGNED AUTO_INCREMENT,
     INDEX dep_ind (department_id),
-    CONSTRAINT FK_department FOREIGN KEY (department_id)
+    CONSTRAINT fk_department FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    frst_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
-    role_id INT NOT NULL,
-    manager_id INT NULL,
-    INDEX role_ind (role_id),
-    CONSTRAINT FK_role FOREIGN KEY (role_id)
-    REFERENCES role(id),
-    INDEX man_ind (manager_id),
-    CONSTRAINT FK_employee FOREIGN KEY (manager_id)
-    REFERENCES role(id)
+    
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT UNSIGNED NOT NULL,
+INDEX role_ind (role_id),
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+manager_id INT UNSIGNED,
+INDEX man_ind (manager_id),
+CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 
 );
 
